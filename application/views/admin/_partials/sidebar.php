30d646463124372e714d544f7fa93abb6d3c68cb
Feisal Ramadhan M <feisalramadhanm@gmail.com>
2019-08-28 11:14:22 +0700

    Main Page Modification
    Updated ...

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -2,7 +2,7 @@
     <ul class="navbar-nav bg-gradient-main sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo base_url('index.php/admin') ?>">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../index.html">
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-chart-area"></i>
         </div>
